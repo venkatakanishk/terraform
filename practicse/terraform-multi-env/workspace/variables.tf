75619e38e33071f6fc571db0238f83367e9c2a08
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-06-14 22:11:35 +0530

    changed something

---
+++ practicse/terraform-multi-env/workspace/variables.tf
@@ -0,0 +1,11 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "instance_type" {
+    default = {
+        dev = "t3.micro"
+        uat = "t3.small"
+        prod = "t3.medium"
+    }
+}
