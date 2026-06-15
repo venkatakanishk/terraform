026f19616c28989aa758a4e7381dcb476d1d6b8b
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-06-15 22:57:35 +0530

    changed something

--- practicse/ec2-module-test/data.tf
+++ practicse/ec2-module-test/data.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "joindevops" {
-  most_recent      = true
-  owners           = ["973714476881"]
+  most_recent = true
+  owners      = ["973714476881"]
 
   filter {
     name   = "name"
--- practicse/ec2-module-test/variables.tf
+++ practicse/ec2-module-test/variables.tf
@@ -1,14 +1,14 @@
 variable "project_name" {
-    default = "roboshop"
+  default = "roboshop"
 }
 
 variable "env" {
-    default = "dev"
+  default = "dev"
 }
 
 variable "sg_ids" {
-    default = "[sg-0bd66f913b1217939]"
+  default = ["sg-0bd66f913b1217939"]
 }
 variable "component" {
-    default = "catalogue"
+  default = "catalogue"
 }
