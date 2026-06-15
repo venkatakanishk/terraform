37e4cb8f36fbaa9d7608fdad8d7c4b0c3cbfc0a0
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-06-15 22:48:37 +0530

    changed something

--- practicse/terraform-aws-instance/ec2.tf
+++ practicse/terraform-aws-instance/ec2.tf
@@ -1,6 +1,6 @@
-resource "aws_instance" "example" {
+resource "aws_instance" "this" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  vpc_security_group_ids = var.sg_ids
   tags = local.ec2_final_tags
 }
