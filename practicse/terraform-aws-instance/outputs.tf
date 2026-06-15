1b9457cc7902c4038297de067de0c907647385c4
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-06-15 22:52:43 +0530

    changed something

--- practicse/terraform-aws-instance/outputs.tf
+++ practicse/terraform-aws-instance/outputs.tf
@@ -2,8 +2,8 @@ output "instance_id" {
   value = aws_instance.this.id
 }
 output "public_ip"{
-    value = aws_instance.this.public.ip
+    value = aws_instance.this.public_ip
 }
 output "private_ip"{
-    value = aws_instance.this.private.ip
+    value = aws_instance.this.private_ip
 }
