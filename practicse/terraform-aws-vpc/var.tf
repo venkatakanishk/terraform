11ac5d17bbf17fc2bdb9325ef356d0db3799123c
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-06-17 22:25:26 +0530

    changed something

---
+++ practicse/terraform-aws-vpc/var.tf
@@ -0,0 +1,14 @@
+variable "project" {
+    type = string
+}
+variable "environment" {
+    type = string
+}
+variable "vpc_cidr" {
+    type = string
+    default = "10.0.0.0/16"
+}
+variable "vpc_tags" {
+    type = map
+    default = {}
+}
